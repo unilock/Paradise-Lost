a4e2e6b492341d69f08c3961ddea39434b284968
unilock <unilock@fennet.rentals>
2024-11-09 20:15:00 -0500

    Remove excess parameters of EntityTrackerEntryMixin#sendPackets
    Fixes mixin conflict on NeoForge via Sinytra Connector
    
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/net/id/paradiselost/mixin/server/EntityTrackerEntryMixin.java
+++ src/main/java/net/id/paradiselost/mixin/server/EntityTrackerEntryMixin.java
@@ -3,18 +3,13 @@ package net.id.paradiselost.mixin.server;
 import net.id.paradiselost.entities.ParadiseLostEntityExtensions;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.network.listener.ClientPlayPacketListener;
-import net.minecraft.network.packet.Packet;
 import net.minecraft.server.network.EntityTrackerEntry;
-import net.minecraft.server.network.ServerPlayerEntity;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.util.function.Consumer;
 
 @Mixin(EntityTrackerEntry.class)
 public class EntityTrackerEntryMixin {
@@ -26,7 +21,7 @@ public class EntityTrackerEntryMixin {
     private Entity entity;
 
     @Inject(method = "sendPackets", at = @At("HEAD"))
-    private void sendPackets(ServerPlayerEntity player, Consumer<Packet<ClientPlayPacketListener>> sender, CallbackInfo ci) {
+    private void sendPackets(CallbackInfo ci) {
         if (this.entity instanceof LivingEntity) {
             this.flipped = ((ParadiseLostEntityExtensions) this.entity).getFlipped();
             this.gravFlippedTime = ((ParadiseLostEntityExtensions) this.entity).getFlipTime();
